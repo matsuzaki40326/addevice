d8e975ea4f7c41f35281dd2f79d01fdbf2fd5868
matsuzaki40326 <pt16pll@gmail.com>
2022-04-06 03:21:14 +0000

    [add]ユーザーページの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @reviews = Review.where(user_id: @user)
   end
 
   def edit
